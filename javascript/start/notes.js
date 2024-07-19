b722991b61b81026b72c54612490ab2f556190e0
EskolaHJ <esk23003@byui.edu>
2024-07-18 18:37:44 -0600

    Re learning old tasks

---
+++ javascript/start/notes.js
@@ -0,0 +1,20 @@
+// Double forward slash comments out
+
+// console keyword refers to an object, a collection of data and action, that we can use in our code
+
+// keywords are built into the JavaScript language
+
+// built into the console object is the .log(), When we write console.log( What is in here will be printed or logged to the console)
+
+console.log(29);
+
+console.log("Hello, world!");
+
+/* the slash star star slash multi comment
+const PI = 3.15;
+const radius = 3;
+let area = 0;
+area = radius * radius * PI;
+radius = 4;
+area = radius * radius * PI;
+*/

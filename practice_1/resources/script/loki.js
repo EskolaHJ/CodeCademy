a031ce684ebae46e629334c2984b47f413769bf9
EskolaHJ <esk23003@byui.edu>
2024-07-18 23:15:05 -0600

    Adding a little JS to the file for fun

---
+++ practice_1/resources/script/loki.js
@@ -0,0 +1,5 @@
+
+const para = document.getElementById("smiteLoki");
+const newText = document.createTextNode("The downside to Loki is the fact that he is an all in god. Once he uses his ultimate if he doesn't kill his target Usually he gets slain.");
+para.appendChild(newText);
+    
